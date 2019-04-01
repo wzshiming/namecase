245db9116a378e6a149e4421075d57bfd2119055
wzshiming <wzshiming@foxmail.com>
2019-04-01 23:16:47 +0800

    remove v1 v2

--- benchmark_test.go
+++ benchmark_test.go
@@ -2,9 +2,6 @@ package namecase
 
 import (
 	"testing"
-
-	nc1 "gopkg.in/wzshiming/namecase.v1"
-	nc2 "gopkg.in/wzshiming/namecase.v2"
 )
 
 func BenchmarkToSnake(b *testing.B) {
@@ -19,38 +16,6 @@ func BenchmarkToHump(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, v := range testdataBascis {
 			ToUpperHump(v)
-		}
-	}
-}
-
-func BenchmarkToSnakeV2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, v := range testdataBascis {
-			nc2.ToLowerSnake(v)
-		}
-	}
-}
-
-func BenchmarkToHumpV2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, v := range testdataBascis {
-			nc2.ToUpperHump(v)
-		}
-	}
-}
-
-func BenchmarkToSnakeV1(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, v := range testdataBascis {
-			nc1.Hump2Snake(v)
-		}
-	}
-}
-
-func BenchmarkToHumpV1(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, v := range testdataBascis {
-			nc1.Snake2Hump(v)
 		}
 	}
 }
